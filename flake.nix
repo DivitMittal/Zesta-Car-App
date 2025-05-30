6ff3f5b80189372ecb239c9d03020c09f9c3c57c
Divit Mittal <64.69.76.69.74.m@gmail.com>
2025-05-31 00:29:08 +0530

    chore: cleanup

--- flake.nix
+++ flake.nix
@@ -1,17 +1,10 @@
 {
   description = "Car License flake";
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    flake-parts.url = "github:hercules-ci/flake-parts";
-    systems.url = "github:nix-systems/default";
-    treefmt-nix = {
-      url = "github:numtide/treefmt-nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-  };
 
-  outputs = {flake-parts, ...} @ inputs:
-    flake-parts.lib.mkFlake {inherit inputs;} {
+  outputs = inputs: let
+    inherit (inputs.flake-parts.lib) mkFlake;
+  in
+    mkFlake {inherit inputs;} {
       systems = builtins.import inputs.systems;
       imports = [inputs.treefmt-nix.flakeModule];
       perSystem.treefmt = {
@@ -32,4 +25,14 @@
         };
       };
     };
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    systems.url = "github:nix-systems/default";
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
 }
