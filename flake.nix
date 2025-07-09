b9c0c2add1c25cc4c7ac46616ca5a639d6441bdb
Divit Mittal <64.69.76.69.74.m@gmail.com>
2025-07-10 02:58:42 +0530

    feat(nix): add devshell, pre-commit-hooks, actions-nix, OS-nixCfg to flake.nix
    Adds devshell for development environment, pre-commit-hooks for
    code formatting, actions-nix for github actions and OS-nixCfg for
    custom library.

--- flake.nix
+++ flake.nix
@@ -1,38 +1,42 @@
 {
-  description = "Car License flake";
+  description = "Zesta Car App's flake";
 
   outputs = inputs: let
     inherit (inputs.flake-parts.lib) mkFlake;
+    specialArgs.customLib = builtins.import (inputs.OS-nixCfg + "/lib/custom.nix") {inherit (inputs.nixpkgs) lib;};
   in
-    mkFlake {inherit inputs;} {
+    mkFlake {inherit inputs specialArgs;} ({inputs, ...}: {
       systems = builtins.import inputs.systems;
-      imports = [inputs.treefmt-nix.flakeModule];
-      perSystem.treefmt = {
-        projectRootFile = "flake.nix";
-
-        flakeCheck = false;
-
-        programs = {
-          #typos.enable = true;
-          ## Nix
-          alejandra.enable = true;
-          deadnix.enable = true;
-          statix.enable = true;
-          ## JSON/JS
-          prettier.enable = true;
-          ## Python
-          black.enable = true;
-        };
-      };
-    };
+      imports = [./flake];
+    });
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
     systems.url = "github:nix-systems/default";
+    devshell = {
+      url = "github:numtide/devshell";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     treefmt-nix = {
       url = "github:numtide/treefmt-nix";
       inputs.nixpkgs.follows = "nixpkgs";
+    };
+    pre-commit-hooks = {
+      url = "github:cachix/git-hooks.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    actions-nix = {
+      url = "github:nialov/actions.nix";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-parts.follows = "flake-parts";
+        pre-commit-hooks.follows = "pre-commit-hooks";
+      };
+    };
+    OS-nixCfg = {
+      url = "github:DivitMittal/OS-nixCfg";
+      flake = false;
     };
   };
 }
